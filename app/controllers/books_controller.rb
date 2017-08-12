430d0de98d65c237de6f6ebcc4c4c914583506f4
Bailey <bailey.g.weathers@gmail.com>
2017-08-12 10:02:19 -0400

    Add Tracers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < BlocWorks::Controller
 
   def welcome
+    puts "inside welcome"
     render :welcome, book: "Eloquent Ruby"
   end
 
   def index
+    puts "inside index"
     render :index, books: Book.all
   end
 end
