8ff26c0df4639cf3339842101129a924ff256e85
Bailey <bailey.g.weathers@gmail.com>
2017-08-13 18:32:58 -0400

    Complete 03-assignment-Architecture-and-Design-Patterns-ORM-BlocBooks

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,13 @@ class BooksController < BlocWorks::Controller
   end
 
   def index
-    puts "gets to index"
     @books = Book.all
+    #binding.pry
     render :index, books: @books
+  end
+
+  def show
+    #binding.pry
+    render :show, book: Book.find(3)
   end
 end
